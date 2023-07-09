08f68e780a465208cdf3802104167c1a54b9142b
Leonardo-dev-br <99846550+Leonardo-dev-br@users.noreply.github.com>
2023-07-09 20:51:41 -0300

    Gerador de Senha v1.0 - Criado

---
+++ script.js
@@ -0,0 +1,30 @@
+let slideElement = document.getElementById("slider");
+let buttonElement = document.getElementById("button");
+let sizePassword = document.getElementById("valor");
+let password = document.getElementById("password");
+let containerPassword = document.getElementById("container-password");
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&";
+let novaSenha = "";
+
+sizePassword.innerHTML = slideElement.value; 
+
+slider.oninput = function () {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+    let pass = "";
+    for(let i = 0, n = charset.length; i < slideElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n)); 
+        }
+
+      containerPassword.classList.remove("hide");  
+      password.innerHTML = pass;
+      novaSenha = pass;
+}
+
+function copyPassaword() {
+    alert("Senha copiada com sucesso!");
+    navigator.clipboard.writeText(novaSenha);
+}
+
